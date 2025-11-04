8b24d633567790ff323e26251d52be9f6097dd69
Gianmarco Zecchinato <87950554+Gianmarco003@users.noreply.github.com>
2025-11-04 12:27:36 +0100

    Firmware: functions optimization and debug
    Update: accelerationCalc()
    Add: all OBD sensor read at power on
    Update: CatalystTemp1 read start after the first engine start
    Add: delay 100ms in the main loop

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -5,6 +5,7 @@
 
 const int rs = 16, en = 19, d0 = 2, d1 = 3, d2 = 4, d3 = 5, d4 = 6, d5 = 7, d6 = 8, d7 = 9;
 LiquidCrystal lcd(rs, en, d0, d1, d2, d3, d4, d5, d6, d7);
+
 char buffer[10];
 
 int obdFuel = 3;
